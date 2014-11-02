e1aab9363f8b85c5de34cffa16d929484708ead0
Jarmo Pakarinen <efigaa@gmail.com>
2014-11-02 12:53:11 +0200

    Added index.php;

--- Model/config.php
+++ Model/config.php
@@ -7,16 +7,17 @@ class Config
 		if($path)
 		{
 			$config = $GLOBALS['config'];
-			$path = explode('/', $config);
+			$path = explode('/', $path);
 
 			foreach ($path as $bit) 
 			{
 				if(isset($config[$bit]))
 				{
 					$config = $config[$bit];
-				}
-				
+				}			
 			}
+
+			return $config;
 		}
 
 		return false;
